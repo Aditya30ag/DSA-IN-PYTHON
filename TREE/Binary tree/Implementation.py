ff0cf3a21d52f05b390c5111b8420a5fb0319317
Aditya30ag <162010355+Aditya30ag@users.noreply.github.com>
2024-04-06 13:07:10 +0530

    Binary tree-1 Implementation

---
+++ TREE/Binary tree/Implementation.py
@@ -0,0 +1,38 @@
+class binary_tree:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+    def add_child(self,data):
+        if self.data==data:
+            return
+
+        if self.data>data:
+            if self.left:
+                self.left.add_child(data)
+
+            else:
+                self.left=binary_tree(data)
+
+        else:
+            if self.right:
+                self.right.add_child(data)
+
+            else:
+                self.right = binary_tree(data)
+
+def build_tree(elements):
+    print("BUILD TREE FOR:",elements)
+    root=binary_tree(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+
+if __name__=="__main__":
+    numbers=[17,4,1,20,9,23,18,34]
+    root=build_tree(numbers)
+    pass
